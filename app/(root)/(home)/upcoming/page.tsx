73de4fa3b0536d4209e487812a436b98f5321ae6
Ishu <kumarishu494@gmail.com>
2025-04-14 10:24:12 +0530

    Upcoming: dead code removed

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,33 +1,9 @@
-"use client";
-
-// import { useState, useEffect } from 'react';
-// import Link from 'next/link';
-// import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import CallList from "@/components/CallList";
 
 export default function UpcomingPage() {
-  // const [meetings, setMeetings] = useState<Call[]>([]);
-  // // const client = new StreamVideoClient({ apiKey, token, user });
-  // const client = useStreamVideoClient();
-
-  // useEffect(() => {
-  //     client?.queryCalls({filter_conditions: {
-  //       ended_at: null
-  //     }}).then(res => setMeetings(res.calls))
-  // }, []);
-
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Upcoming</h1>
-      {/* <nav className='flex flex-col gap-2'>
-        {meetings.map(meeting => {
-          const id = meeting.cid.split(':')[1];
-          
-          return (
-            <Link target='_blank' href={`/meeting/${id}`} key={id}>{id}</Link>
-          )
-        })}
-      </nav> */}
       <CallList type="upcoming" />
     </section>
   )
